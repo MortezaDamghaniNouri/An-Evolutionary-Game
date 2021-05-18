77548fa624a3ae91b3898832e723337a196195c2
Morteza-Damghani <mortezadamghani77@gmail.com>
2021-05-18 09:47:32 -0700

    Qualification function completed and tested.

--- Main.py
+++ Main.py
@@ -16,74 +16,76 @@ def random_chromosome_generator(chromosome_num, length):
     return random_chromosomes
 
 
+# This function finds the maximum of an input list
+def max_finder(user_input_array):
+    i = 1
+    maximum = user_input_array[0]
+    while i < len(user_input_array):
+        if user_input_array[i] > maximum:
+            maximum = user_input_array[i]
+        i += 1
+    return maximum
+
+
+# This function determines if the player is up or down
+# Location input argument in this function determines the location where we want to determine the play is up or down (down: on the ground and up: in the air)
+def up_or_down(input_chromosome, location, start_location):
+    if location == start_location:
+        return "down"
+    one_counter = 0
+    i = location - 1
+    while i >= start_location:
+        if input_chromosome[i] == 1:
+            one_counter += 1
+            i = i - 1
+        else:
+            break
+    if one_counter % 2 == 0:
+        return "down"
+    else:
+        return "up"
+
+
 # This function finds the qualification os an input chromosome
 def qualification_finder(input_chromosome, user_input_array):
     position = 0
     points_without_loses = []
     eaten_mushrooms_number = 0
-    start_position = 0
+    start_location = 0
     point = 0
-    while position < len(user_input_array):
-        if user_input_array[position + 1] == '_':
-            # Redundant jumps
-            if position != start_position and user_input_array[position] == 'G' and input_chromosome[position] == 1:
-                point += -0.5
+    while position < len(user_input_array) - 1:
+        print("The position is: " + str(position))
+        print("The point is: " + str(point))
+        print("The points_without_loses: " + str(points_without_loses))
+        print("Eaten: " + str(eaten_mushrooms_number))
+        print("===========")
+        # Redundant jumps
+        if user_input_array[position] != 'G' and up_or_down(input_chromosome, position, start_location) == "up":
+            point += -0.5
 
-            else:
-                if position + 2 < len(user_input_array) and user_input_array[position + 2] != 'G' and input_chromosome[position] == 1:
-                    point += -0.5
-
-            # Adding the point of mushroom
-            if position - 1 >= start_position and input_chromosome[position - 1] != 1 and user_input_array[position] == 'M':
-                eaten_mushrooms_number += 1
-
+        # Adding the point of mushroom
+        if user_input_array[position] == 'M' and up_or_down(input_chromosome, position, start_location) == "down":
+            eaten_mushrooms_number += 1
 
         if user_input_array[position + 1] == 'G':
-            if input_chromosome[position] != 1 and position - 1 >= start_position and input_chromosome[position - 1] != 1:
+            if input_chromosome[position] != 1 and position - 1 >= start_location and input_chromosome[position - 1] != 1:
                 # If a lose happen this part of the code will execute
-                point += (position - start_position) + eaten_mushrooms_number * 2
-                start_position = position + 1
+                point += (position - start_location) + eaten_mushrooms_number * 2
+                start_location = position + 1
                 points_without_loses.append(point)
                 eaten_mushrooms_number = 0
                 point = 0
             # Killing Goumpas is implemented here and this action will add two extra points to the user's points
-            if position - 1 >= start_position and input_chromosome[position - 1] == 1 and input_chromosome[position] == 0:
+            if up_or_down(input_chromosome, position, start_location) == "up":
                 point += 2
             # Losing because of GG which means two adjacent Goumpa
-            if user_input_array[position] == 'G' and start_position != position:
+            if user_input_array[position] == 'G' and start_location != position:
                 # If a lose happen this part of the code will execute
-                point += (position - start_position) + eaten_mushrooms_number * 2
-                start_position = position + 1
+                point += (position - start_location) + eaten_mushrooms_number * 2
+                start_location = position + 1
                 points_without_loses.append(point)
                 eaten_mushrooms_number = 0
                 point = 0
-
-            # Adding the point of mushroom
-            if position - 1 >= start_position and input_chromosome[position - 1] != 1 and user_input_array[position] == 'M':
-                eaten_mushrooms_number += 1
-
-            # Killing Goumpas is implemented here and this action will add two extra points to the user's points
-            if user_input_array[position] == 'M' and position - 1 >= start_position and input_chromosome[position - 1] == 1:
-                point += 2
-
-
-
-
-        if user_input_array[position + 1] == 'M':
-            # Redundant jumps
-            if position != start_position and user_input_array[position] == 'G' and input_chromosome[position] == 1:
-                point += -0.5
-
-            else:
-                if position + 2 < len(user_input_array) and user_input_array[position + 2] != 'G' and input_chromosome[position] == 1:
-                    point += -0.5
-
-            # Adding the point of mushroom
-            if position - 1 >= start_position and input_chromosome[position - 1] != 1 and user_input_array[position] == 'M':
-                eaten_mushrooms_number += 1
-
-
-
 
 
 
@@ -91,41 +93,50 @@ def qualification_finder(input_chromosome, user_input_array):
 
 
         if user_input_array[position + 1] == 'L':
-            if position - 1 >= start_position and input_chromosome[position - 1] == 1:
+            if position - 1 >= start_location and input_chromosome[position - 1] == 1:
                 # If a lose happen this part of the code will execute
-                point += (position - start_position) + eaten_mushrooms_number * 2
-                start_position = position + 1
+                point += (position - start_location) + eaten_mushrooms_number * 2
+                start_location = position + 1
                 points_without_loses.append(point)
                 eaten_mushrooms_number = 0
                 point = 0
 
-            if input_chromosome[position] != 2 and position - 1 >= start_position and input_chromosome[position - 1] != 1:
+            if input_chromosome[position] != 2 and position - 1 >= start_location and input_chromosome[position - 1] != 1:
                 # If a lose happen this part of the code will execute
-                point += (position - start_position) + eaten_mushrooms_number * 2
-                start_position = position + 1
+                point += (position - start_location) + eaten_mushrooms_number * 2
+                start_location = position + 1
                 points_without_loses.append(point)
                 eaten_mushrooms_number = 0
                 point = 0
 
             # Losing because of GL which means two adjacent Goumpa and Luckipou
-            if user_input_array[position] == 'G' and start_position != position:
+            if user_input_array[position] == 'G' and start_location != position:
                 # If a lose happen this part of the code will execute
-                point += (position - start_position) + eaten_mushrooms_number * 2
-                start_position = position + 1
+                point += (position - start_location) + eaten_mushrooms_number * 2
+                start_location = position + 1
                 points_without_loses.append(point)
                 eaten_mushrooms_number = 0
                 point = 0
 
-            # Adding the point of mushroom
-            if position - 1 >= start_position and input_chromosome[position - 1] != 1 and user_input_array[position] == 'M':
-                eaten_mushrooms_number += 1
-
-
 
         position += 1
 
+    # Adding the point of finishing the map by jump
+    if up_or_down(input_chromosome, len(user_input_array) - 1, start_location) == "up":
+        point += 1
 
+    if start_location == 0:
+        point += 5
+        point += (position - start_location) + eaten_mushrooms_number * 2
+        return point
 
+    point += (position - start_location) + eaten_mushrooms_number * 2
+    points_without_loses.append(point)
+
+    print(points_without_loses)
+
+    # Finding the maximum point which is achieved by the chromosome
+    return max_finder(points_without_loses)
 
 
 # This is the main function of the code which the game executes in
